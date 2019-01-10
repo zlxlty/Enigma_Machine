1d4ad3f30f0163eed847adc63b22fd3bfe3a383a
SkyLu <2924312854@qq.com>
2019-01-10 08:05:39 +0800

    add cmake

---
+++ src/CMakeFiles/Enigma.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/skylu/Desktop/Enigma_1.0.0/src/input.c" "/home/skylu/Desktop/Enigma_1.0.0/src/CMakeFiles/Enigma.dir/input.c.o"
+  "/home/skylu/Desktop/Enigma_1.0.0/src/linked_list.c" "/home/skylu/Desktop/Enigma_1.0.0/src/CMakeFiles/Enigma.dir/linked_list.c.o"
+  "/home/skylu/Desktop/Enigma_1.0.0/src/main.c" "/home/skylu/Desktop/Enigma_1.0.0/src/CMakeFiles/Enigma.dir/main.c.o"
+  "/home/skylu/Desktop/Enigma_1.0.0/src/rotorgroup.c" "/home/skylu/Desktop/Enigma_1.0.0/src/CMakeFiles/Enigma.dir/rotorgroup.c.o"
+  "/home/skylu/Desktop/Enigma_1.0.0/src/rotors.c" "/home/skylu/Desktop/Enigma_1.0.0/src/CMakeFiles/Enigma.dir/rotors.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/Enigma.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Enigma.dir/input.c.o"
+  "CMakeFiles/Enigma.dir/linked_list.c.o"
+  "CMakeFiles/Enigma.dir/main.c.o"
+  "CMakeFiles/Enigma.dir/rotorgroup.c.o"
+  "CMakeFiles/Enigma.dir/rotors.c.o"
+  "Enigma.pdb"
+  "Enigma"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Enigma.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
